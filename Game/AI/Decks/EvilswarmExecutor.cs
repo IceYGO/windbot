96bc0f0c92efdec61e48cab843f34fa40a2f749e
mercury233 <me@mercury233.me>
2016-07-28 03:54:57 +0800

    init near deck2 3

---
+++ Game/AI/Decks/EvilswarmExecutor.cs
@@ -0,0 +1,39 @@
+﻿namespace WindBot.Game.AI.Decks
+{
+    [Deck("Evilswarm", "AI_Evilswarm")]
+    public class EvilswarmExecutor : DefaultExecutor
+    {
+        public enum CardId
+        {
+            黑洞 = 53129443,
+            宇宙旋风 = 8267140,
+            侵略的泛发感染 = 27541267,
+            神之宣告 = 41420027,
+            神之警告 = 84749824,
+            神之通告 = 40605147
+        }
+
+        public EvilswarmExecutor(GameAI ai, Duel duel)
+            : base(ai, duel)
+        {
+            AddExecutor(ExecutorType.Activate, (int)CardId.黑洞, DefaultDarkHole);
+            AddExecutor(ExecutorType.Activate, (int)CardId.宇宙旋风, DefaultMysticalSpaceTyphoon);
+            AddExecutor(ExecutorType.Activate, (int)CardId.神之宣告, DefaultTrap);
+            AddExecutor(ExecutorType.Activate, (int)CardId.神之警告, DefaultTrap);
+            AddExecutor(ExecutorType.Activate, (int)CardId.神之通告, DefaultTrap);
+            AddExecutor(ExecutorType.SpellSet, (int)CardId.侵略的泛发感染);
+            AddExecutor(ExecutorType.Activate, DontChainMyself);
+            AddExecutor(ExecutorType.Summon);
+            AddExecutor(ExecutorType.SpSummon);
+            AddExecutor(ExecutorType.Repos, DefaultMonsterRepos);
+            AddExecutor(ExecutorType.SpellSet);
+        }
+
+        private bool DontChainMyself()
+        {
+            return LastChainPlayer != 0;
+        }
+
+        // will be added soon...?
+    }
+}
---
+++ Game/AI/Decks/GraydleExecutor.cs
@@ -0,0 +1,37 @@
+﻿namespace WindBot.Game.AI.Decks
+{
+    [Deck("Graydle", "AI_Graydle")]
+    public class GraydleExecutor : DefaultExecutor
+    {
+        public enum CardId
+        {
+            黑洞 = 53129443,
+            宇宙旋风 = 8267140,
+            神之宣告 = 41420027,
+            神之警告 = 84749824,
+            神之通告 = 40605147
+        }
+
+        public GraydleExecutor(GameAI ai, Duel duel)
+            : base(ai, duel)
+        {
+            AddExecutor(ExecutorType.Activate, (int)CardId.黑洞, DefaultDarkHole);
+            AddExecutor(ExecutorType.Activate, (int)CardId.宇宙旋风, DefaultMysticalSpaceTyphoon);
+            AddExecutor(ExecutorType.Activate, (int)CardId.神之宣告, DefaultTrap);
+            AddExecutor(ExecutorType.Activate, (int)CardId.神之警告, DefaultTrap);
+            AddExecutor(ExecutorType.Activate, (int)CardId.神之通告, DefaultTrap);
+            AddExecutor(ExecutorType.Activate, DontChainMyself);
+            AddExecutor(ExecutorType.MonsterSet);
+            AddExecutor(ExecutorType.SpSummon);
+            AddExecutor(ExecutorType.Repos, DefaultMonsterRepos);
+            AddExecutor(ExecutorType.SpellSet);
+        }
+
+        private bool DontChainMyself()
+        {
+            return LastChainPlayer != 0;
+        }
+
+        // will be added soon...?
+    }
+}
