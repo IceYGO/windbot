f0072db24ab4e36ef5643acfe35f37eae167729b
mercury233 <me@mercury233.me>
2017-01-31 04:18:31 +0800

    fix

--- Game/AI/Decks/QliphortExecutor.cs
+++ Game/AI/Decks/QliphortExecutor.cs
@@ -137,6 +137,10 @@ namespace WindBot.Game.AI.Decks
 
         public override IList<ClientCard> OnSelectCard(IList<ClientCard> cards, int min, int max, bool cancelable)
         {
+            if (max <= min)
+            {
+                return null;
+            }
             IList<ClientCard> selected = new List<ClientCard>();
 
             // select the last cards
