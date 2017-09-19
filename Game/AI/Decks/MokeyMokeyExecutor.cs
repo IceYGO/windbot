deda84bb75697799bf853378bc61ee95f35099bd
mercury233 <me@mercury233.me>
2017-09-19 13:41:38 +0800

    add new ai

---
+++ Game/AI/Decks/MokeyMokeyExecutor.cs
@@ -0,0 +1,42 @@
+﻿using YGOSharp.OCGWrapper.Enums;
+using System.Collections.Generic;
+using WindBot;
+using WindBot.Game;
+using WindBot.Game.AI;
+
+namespace WindBot.Game.AI.Decks
+{
+    [Deck("MokeyMokey", "AI_MokeyMokey")]
+    public class MokeyMokeyExecutor : DefaultExecutor
+    {
+        public enum CardId
+        {
+            LeoWizard = 4392470,
+            Bunilla = 69380702
+        }
+
+        private int RockCount = 0;
+
+        public MokeyMokeyExecutor(GameAI ai, Duel duel)
+            : base(ai, duel)
+        {
+            AddExecutor(ExecutorType.Summon);
+            AddExecutor(ExecutorType.Repos, DefaultMonsterRepos);
+            AddExecutor(ExecutorType.SpellSet);
+        }
+
+        public override int OnRockPaperScissors()
+        {
+            RockCount++;
+            if (RockCount <= 3)
+                return 2;
+            else
+                return base.OnRockPaperScissors();
+        }
+
+        public override bool OnPreBattleBetween(ClientCard attacker, ClientCard defender)
+        {
+            return attacker.Attack > 0;
+        }
+    }
+}
