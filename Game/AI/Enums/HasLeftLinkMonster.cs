e95f94d4144b175b7672ed4dd1f14437420a2f2a
handsomekiwi <36762809+handsomekiwi@users.noreply.github.com>
2018-05-03 09:49:15 +0800

    add linkzonemonster

---
+++ Game/AI/Enums/HasLeftLinkMonster.cs
@@ -0,0 +1,10 @@
+﻿namespace WindBot.Game.AI.Enums
+{
+    /// <summary>
+    /// Link Monster that has left link marker
+    /// </summary>
+    public enum HasLeftLinkMonster
+    {
+        
+    }
+}
---
+++ Game/AI/Enums/HasRightDownLinkMonster.cs
@@ -0,0 +1,10 @@
+﻿namespace WindBot.Game.AI.Enums
+{
+    /// <summary>
+    /// Link Monster that has right down link marker
+    /// </summary>
+    public enum HasRightDownLinkMonster
+    {
+        
+    }
+}
