693ab8be39e74725bd9bf94966f418b443732b38
Wind2009-Louse <wind.jackey@qq.com>
2018-08-11 11:32:31 +0800

    Revert

--- Logger.cs
+++ Logger.cs
@@ -12,18 +12,6 @@ namespace WindBot
         {
 #if DEBUG
             Console.WriteLine("[" + DateTime.Now.ToString("yy-MM-dd HH:mm:ss") + "] " + message);
-<<<<<<< HEAD
-            using (FileStream fs = new FileStream(@Path.GetFullPath("log.txt"), FileMode.OpenOrCreate, FileAccess.Write))
-              {
-                  using (StreamWriter sw = new StreamWriter(fs))
-                 {
-                     sw.BaseStream.Seek(0, SeekOrigin.End);
-                     sw.WriteLine("{0}", "[" + DateTime.Now.ToString("yy-MM-dd HH:mm:ss") + "] " + message);
-                     sw.Flush();
-                 }
-             }
-=======
->>>>>>> parent of 61647f1... Logger output
 #endif
         }
         public static void WriteErrorLine(string message)
