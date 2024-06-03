eb5d8f489caf699de078326b8098b6dd2c44ba79
mercury233 <me@mercury233.me>
2024-06-03 15:52:46 +0800

    version

--- WindBotInfo.cs
+++ WindBotInfo.cs
@@ -24,7 +24,7 @@ namespace WindBot
             Host = "127.0.0.1";
             Port = 7911;
             HostInfo = "";
-            Version = 0x1360;
+            Version = 0x1361;
             Hand = 0;
             Debug = false;
             Chat = true;
