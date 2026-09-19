7c1c17f13d5c7639df87539a5d04c23f595195f1
Wind2009-Louse <wind.jackey@qq.com>
2026-09-19 20:54:10 +0800

    Add Elfnote

--- Game/AI/DefaultExecutor.cs
+++ Game/AI/DefaultExecutor.cs
@@ -250,6 +250,7 @@ namespace WindBot.Game.AI
 
             public const int LoThePrayersOfTheVoicelessVoice = 25801745;
             public const int BarrierOfTheVoicelessVoice = 98477480;
+            public const int ElfnotesRhapsodiaOfMadness = 24092792;
 
             public const int DiabellzeOfTheOriginalSin = 53765052;
             public const int PotOfExtravagance = 49238328;
@@ -535,9 +536,16 @@ namespace WindBot.Game.AI
             if (defender.IsCode(_CardId.RescueACEHydrant) && !defender.IsDisabled() && Enemy.GetMonsters().Any(monster => monster.HasSetcode(_Setcode.RescueACE) && !monster.IsCode(_CardId.RescueACEHydrant)))
                 return false;
 
-            if (Enemy.HasInSpellZone(_CardId.BarrierOfTheVoicelessVoice, true) && Enemy.HasInMonstersZone(_CardId.LoThePrayersOfTheVoicelessVoice, faceUp: true)
+            if (Enemy.HasInSpellZone(_CardId.ElfnotesRhapsodiaOfMadness, true) && Enemy.HasInMonstersZone(_CardId.LoThePrayersOfTheVoicelessVoice, faceUp: true)
                 && Enemy.GetMonsters().Any(card => card.HasType(CardType.Ritual) && card.IsFaceup()) && !defender.HasType(CardType.Ritual))
                 return false;
+
+            if (Enemy.HasInSpellZone(_CardId.ElfnotesRhapsodiaOfMadness, true))
+            {
+                ClientCard centerMonster = Enemy.MonsterZone[2];
+                if (centerMonster != null && defender != centerMonster)
+                    return false;
+            }
 
             return true;
         }
